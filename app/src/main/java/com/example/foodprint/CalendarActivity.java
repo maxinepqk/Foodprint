b85d325edd96b098eac4ba259bddd3aaece9b1df
Swara Srinivasan <31788135+swaras16@users.noreply.github.com>
2019-02-09 08:38:23 -0500

    date + goes to calendar

---
+++ app/src/main/java/com/example/foodprint/CalendarActivity.java
@@ -0,0 +1,55 @@
+package com.example.foodprint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
+
+    TextView thedate;
+    TextView btngocalendar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/" + dayOfMonth;
+                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+                Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+
+            }
+        });
+
+        // date and calendar view
+        thedate = (TextView) findViewById(R.id.date);
+        btngocalendar = (Button) findViewById(R.id.btngocalendar);
+
+        Intent incoming = getIntent();
+        String date = incoming.getStringExtra("date");
+        thedate.setText(date);
+
+        btngocalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarActivity.this,CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
