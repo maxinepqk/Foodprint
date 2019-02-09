e36b5a3b0d5e361b2cbcfceb9ab93d2690b82f8a
maxinepqk <maxinepqk@gmail.com>
2019-02-08 19:41:08 -0500

    Added basic to-do list with add and remove functions

--- app/src/main/java/com/example/foodprint/MainActivity.java
+++ app/src/main/java/com/example/foodprint/MainActivity.java
@@ -1,31 +1,74 @@
 package com.example.foodprint;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // declaring stateful objects here; these will be null before onCreate is called
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // obtain a reference to the ListView created with the layout
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        // initialize the items list
+        items = new ArrayList<>();
+        // initialize the adapter using the items list
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        // wire the adapter to the view
+        lvItems.setAdapter(itemsAdapter);
+
+        // add some mock items to the list
+        items.add("First todo item");
+        items.add("Second todo item");
+
+        // setup the listener on creation
+        setupListViewListener();
+    }
+
+    private void setupListViewListener() {
+        // set the ListView's itemLongClickListener
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // remove the item in the list at the index given by position
+                items.remove(position);
+                // notify the adapter that the underlying dataset changed
+                itemsAdapter.notifyDataSetChanged();
+                // return true to tell the framework that the long click was consumed
+                return true;
             }
         });
+    }
+
+    public void onAddItem(View v) {
+        // obtain a reference to the EditText created with the layout
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        // grab the EditText's content as a String
+        String itemText = etNewItem.getText().toString();
+        // add the item to the list via the adapter
+        itemsAdapter.add(itemText);
+        // clear the EditText by setting it to an empty String
+        etNewItem.setText("");
+        // display a notification to the user
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
