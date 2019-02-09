b10af11d43125bc26cedd9456de3e93c27fed0f6
Swara Srinivasan <31788135+swaras16@users.noreply.github.com>
2019-02-09 01:29:01 -0500

    update to calendar fragment

--- app/src/main/java/com/example/foodprint/CalendarFragment.java
+++ app/src/main/java/com/example/foodprint/CalendarFragment.java
@@ -7,6 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -100,4 +106,27 @@ public class CalendarFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+    
+    public class CalendarActivity extends AppCompatActivity {
+
+    private  static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/"+ dayOfMonth ;
+                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+                Intent intent = new Intent(CalendarActivity.this,MainActivity.class);
+                intent.putExtra("date",date);
+                startActivity(intent);
+
+            }
+        });
+    }
+    
 }
